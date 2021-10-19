1e653252dfb52c9966898f71a9a628f47b580552
jooomOut <rimeilo324@naver.com>
2021-10-19 21:06:17 +0900

    Feat: album에서 feed 사용

--- src/pages/album/index.js
+++ src/pages/album/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import AppLayout from '../../component/layout/AppLayout';
 import HomeLayout from '../../component/layout/HomeLayout';
+import Feed from "../../component/Feed";
 
 export default function AlbumPage() {
+    const feeds = [
+        {
+            img_url: "https://upload.wikimedia.org/wikipedia/commons/f/f8/190611_%EC%9D%B4%EB%82%98%EA%B2%BD.jpg",
+            tags: ['이런', '데이터' , '들이?','있겠조?','이나경!']
+        },
+        {
+            img_url: "https://upload.wikimedia.org/wikipedia/commons/f/f8/190611_%EC%9D%B4%EB%82%98%EA%B2%BD.jpg",
+            tags: ['이런', '데이터' , '들이?','있겠조?','이나경!']
+        },
+    ]
   return (
     <>
       <AppLayout>
         <HomeLayout>
-          내 앨범 페이지
+            {feeds.map((feed) => {
+                return <Feed feed={feed}/>
+            })}
         </HomeLayout>
       </AppLayout>
     </>
