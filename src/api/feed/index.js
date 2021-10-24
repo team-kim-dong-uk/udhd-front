bcd932d60f70c7a320909d0e290a85acc101de58
jooomOut <rimeilo324@naver.com>
2021-10-24 14:29:08 +0900

    Feat: feed like 기능 redux, api 추가

--- src/api/feed/index.js
+++ src/api/feed/index.js
@@ -3,3 +3,11 @@ import client from '../client';
 export const getFeeds = () => {
     return client.get(`feeds`);
 }
+
+export const addFeedList = ({feedId}) => {
+    return client.put(`feeds/${feedId}/like`)
+}
+
+export const deleteFeedList = ({feedId}) => {
+    return client.delete(`feeds/${feedId}/like`)
+}
