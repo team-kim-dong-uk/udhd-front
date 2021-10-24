b188b4a23998ced1dc14f3bb528a3c44818d7c0f
dongjinBaek <dj9136@gmail.com>
2021-10-24 13:00:34 +0900

    feat: feed redux 추가

--- src/core/reducer/index.js
+++ src/core/reducer/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import auth from '../redux/auth';
+import feed from '../redux/feed';
+import loading from '../redux/loading';
 
-const rootReducer = combineReducers({auth});
+const rootReducer = combineReducers({auth, feed, loading});
 export default rootReducer;
--- src/core/saga/index.js
+++ src/core/saga/index.js
@@ -1,7 +1,10 @@
 import { all } from 'redux-saga/effects';
 import { authSaga } from '../redux/auth';
+import { feedSaga
+ } from '../redux/feed';
 export default function* rootSaga() {
   yield all([
     authSaga(),
+    feedSaga(),
   ]);
 }
