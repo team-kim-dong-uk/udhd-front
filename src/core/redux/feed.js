ac20589e7a052dea9f057628dc838594a05ef272
jooomOut <rimeilo324@naver.com>
2021-10-25 20:44:09 +0900

    Feat: like, save 로 feed 가져오는 redux, api 구현

--- src/core/redux/feed.js
+++ src/core/redux/feed.js
@@ -11,6 +11,9 @@ const prefix = 'feed/';
 
 // 2. 액션타입에 대해서 정의합니다.
 const GET_FEEDS = asyncActionCreator(`${prefix}GET_FEEDS`);
+const GET_FEEDS_LIKE = asyncActionCreator(`${prefix}GET_FEEDS_LIKE`);
+const GET_FEEDS_SAVE = asyncActionCreator(`${prefix}GET_FEEDS_SAVE`);
+
 const ADD_FEED_LIKE = asyncActionCreator(`${prefix}ADD_FEED_LIKE`);
 const DEL_FEED_LIKE = asyncActionCreator(`${prefix}DEL_FEED_LIKE`);
 
@@ -19,7 +22,9 @@ const UNSAVE_FEED = asyncActionCreator(`${prefix}UNSAVE_FEED`);
 
 // 3. 액션함수에 대해서 정의합니다.
 //photoId: optional
-export const getFeeds = createAsyncAction(GET_FEEDS, ({photoId}) => ({photoId}));
+export const getFeeds = createAsyncAction(GET_FEEDS);
+export const getFeedsLike = createAsyncAction(GET_FEEDS_LIKE, ({type, userId, count, page}) => ({type, userId, count, page}));
+export const getFeedsSave = createAsyncAction(GET_FEEDS_SAVE, ({type, userId, count, page}) => ({type, userId, count, page}));
 
 export const addFeedLike = createAsyncAction(ADD_FEED_LIKE, ({feedId}) => ({feedId}));
 export const deleteFeedLike = createAsyncAction(DEL_FEED_LIKE, ({feedId}) => ({feedId}));
@@ -29,6 +34,9 @@ export const unsaveFeed = createAsyncAction(UNSAVE_FEED, ({feedId}) => ({feedId}
 
 // 4. saga 비동기 관련 함수가 필요할 경우 작성 합니다. (optional) saga함수들의 모음은 최하단에 나열합니다.
 const getFeedsSaga = createAsyncSaga(getFeeds, feedAPI.getFeeds);
+const getFeedsLikeSaga = createAsyncSaga(getFeedsLike, feedAPI.getFeedsByType);
+const getFeedsSaveSaga = createAsyncSaga(getFeedsSave, feedAPI.getFeedsByType);
+
 const addFeedLikeSaga = createAsyncSaga(addFeedLike, feedAPI.addFeedLike);
 const deleteFeedLikeSaga = createAsyncSaga(deleteFeedLike, feedAPI.deleteFeedLike);
 const saveFeedSaga = createAsyncSaga(saveFeed, feedAPI.saveFeed);
@@ -36,11 +44,21 @@ const unsaveFeedSaga = createAsyncSaga(unsaveFeed, feedAPI.unsaveFeed);
 
 // 5. 초기 상태 정의
 const initialState = {
-  data: [],
+  feeds: {
+    data: [],
+    error: null
+  },
+  feedsLike: {
+    data: [],
+    error: null
+  },
+  feedsSave: {
+    data: [],
+    error: null
+  },
   isEnd: false,
   loading: false,
   error: {
-      feed: null,
       addLike: null,
       deleteLike: null,
       saveFeed: null,
@@ -54,17 +72,55 @@ export default handleActions(
       [GET_FEEDS.SUCCESS]: (state, action) => {
           return {
               ...state,
-              data: 1,
+              feeds: {
+                  data: 1,
+                  error: null,
+              },
           };
       },
       [GET_FEEDS.FAILURE]: (state, action) => {
         return {
             ...state,
-            error: {
-                ...state.error,
-                feed: action.payload.error,
+            feeds: {
+                error: action.payload.error,
             },
         };
+      },
+      [GET_FEEDS_LIKE.SUCCESS]: (state, action) => {
+          return {
+              ...state,
+              feedsLike: {
+                  data: 1,
+                  error: null,
+              },
+          };
+      },
+      [GET_FEEDS_LIKE.FAILURE]: (state, action) => {
+          return {
+              ...state,
+              feedsLike: {
+                  data: state.feedsLike.data,
+                  error: action.payload.error,
+              },
+          };
+      },
+      [GET_FEEDS_SAVE.SUCCESS]: (state, action) => {
+          return {
+              ...state,
+              feedsSave: {
+                  data: 1,
+                  error: null,
+              },
+          };
+      },
+      [GET_FEEDS_SAVE.FAILURE]: (state, action) => {
+          return {
+              ...state,
+              feedsSave: {
+                  data: state.feedsSave.data,
+                  error: action.payload.error,
+              },
+          };
       },
       [ADD_FEED_LIKE.FAILURE]: (state, action) => {
           return {
@@ -109,6 +165,8 @@ export default handleActions(
 // 7. `4`번에서 작성한 saga함수들에 대해 구독 요청에 대한 정의를 최하단에 해주도록 합니다.
 export function* feedSaga() {
     yield takeEvery(GET_FEEDS.REQUEST, getFeedsSaga);
+    yield takeEvery(GET_FEEDS_LIKE.REQUEST, getFeedsLikeSaga);
+    yield takeEvery(GET_FEEDS_SAVE.REQUEST, getFeedsSaveSaga);
     yield takeEvery(ADD_FEED_LIKE.REQUEST, addFeedLikeSaga);
     yield takeEvery(DEL_FEED_LIKE.REQUEST, deleteFeedLikeSaga);
     yield takeEvery(SAVE_FEED.REQUEST, saveFeedSaga);
