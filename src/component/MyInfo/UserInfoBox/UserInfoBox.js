9da7024507aaab9ad5c1c7f88db89b104cd7c996
jooomOut <rimeilo324@naver.com>
2021-11-01 16:08:08 +0900

    Style: Mypage 없는 사진

--- src/component/MyInfo/UserInfoBox/UserInfoBox.js
+++ src/component/MyInfo/UserInfoBox/UserInfoBox.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components';
 import {useSelector} from "react-redux";
 import Setting from '../../../../assets/setting.svg';
 import Link from 'next/link'
 import logo from '../../../../assets/drawable-xhdpi/symbol_black.webp'
 import Image from "next/image";
+import {getFeedsLike} from "../../../core/redux/feed";
 
 
 /*
