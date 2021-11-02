c58cb107e97b09e1bf1ee6a95881be5b1fa8fc32
dongjinBaek <dj9136@gmail.com>
2021-11-02 13:02:56 +0900

    feat: add privacy policy page

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useEffect } from "react";
+export default function privacyPolicy() {
+  
+  return (
+    <div>
+      <a href='https://www.privacypolicygenerator.info/live.php?token=kyk9SPyuCI7XzEtxqcnkSIvEjcY6d3Ww'>click to view privacy policy...</a>
+    </div>
+  );
+};
