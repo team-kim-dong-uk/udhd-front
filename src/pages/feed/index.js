5c43deeb747e6fbca87c0cb22d67ccbaa9613094
dongjinBaek <dj9136@gmail.com>
2021-11-02 20:41:00 +0900

    feat: main feed title 추가

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -5,6 +5,7 @@ import Feed from "../../component/Feed";
 import {useDispatch, useSelector} from "react-redux";
 import { loginSuccess } from '../../core/redux/auth';
 import { getFeeds } from '../../core/redux/feed';
+import styled from 'styled-components';
 
 export default function FeedPage() {
     const { feed, loading } = useSelector(state => state);
@@ -16,9 +17,21 @@ export default function FeedPage() {
     <>
       <AppLayout>
         <HomeLayout>
+            <S.Title>Weekly 20</S.Title>
             <Feed data={feed?.feeds?.data ? feed.feeds.data : []} showTitle={true}/>
         </HomeLayout>
       </AppLayout>
     </>
   )
 }
+
+const S = {};
+S.Title = styled.div`
+  height: 40px;
+  display: flex !important;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  font-size: 20px;
+  font-weight: bold;
+`;
