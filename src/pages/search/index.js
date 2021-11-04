3ebbd4688314a248fe0e58465379a34644e4eaf0
jooomOut <rimeilo324@naver.com>
2021-11-04 15:48:33 +0900

    Feat: 추천 태그 하드코딩

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -7,16 +7,17 @@ import SearchPhotoGrid from '../../component/PhotoGrid/SearchPhotoGrid';
 import { getRandomPhotos } from '../../core/redux/photos';
 import { initAmplitude, sendAmplitudeData, setAmplitudeUserId } from '../../util/amplitude';
 import Tags from "../../component/Feed/FeedItem/Tags";
+import tags from "../../core/redux/tags";
 
 export default function SearchPage() {
   const dispatch = useDispatch();
   const { photos, auth } = useSelector(state => state);
 
-  const tagsData = {
+  let tagsData = {
       tags: [
-          "효정", '미미', '유아', '승희', '지호', '비니', '아린'
-      ]
-  }
+          '단체', "효정", '미미', '유아', '승희', '지호', '비니', '아린'
+      ].concat(['직찍', '인별', '비하인드', '공식', '화보', '네이버포스트', 'MV'].sort(() => Math.random() - 0.5).slice(0, 3))
+  };
 
   useEffect(() => {
       initAmplitude();
